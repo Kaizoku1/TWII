a7641066056cf49300f732ecd55626a4b09a799c
Dan Yerena <danyerena12@gmail.com>
2021-09-09 17:20:50 -0500

    Laravel Instalado

---
+++ blog/resources/views/archivo.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nombre</title>
+</head>
+<body>
+	<h1>Probando Sumas</h1>
+
+	@php
+		$suma = 5+2;
+	@endphp
+
+	<h3>Resultado: {{$suma}}</h3>
+
+</body>
+</html>
